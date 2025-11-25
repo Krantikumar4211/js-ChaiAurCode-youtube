26a96cdee04bf77460499d543a5330186c48f543
Krantikumar Patil <krantikumar4211@gmail.com>
2025-11-25 05:13:06 +0000

    datatypes

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"  //treat all JS code as new version
+
+// alert(3 +3 );  //we are using node.js not browser
+
+// console.log(3
+//     +
+//     3) // code readability shoud be high
+
+// notes link = tc39.es/  
+
+let name ="KK"
+let age = 23
+let isLogging = false
+
+//number => 2 to power 53
+//bigint
+//String =>  "" or '' but prefer ""
+//boolean => true/false
+//null  -> standalone value
+//undefined => 
+//symbol => unique
+
+//object 
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
