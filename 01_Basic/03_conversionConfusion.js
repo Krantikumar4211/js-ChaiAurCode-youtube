dac1f0d262420406e916eb7d0c067e6931c682a8
Krantikumar Patil <krantikumar4211@gmail.com>
2025-11-25 05:30:56 +0000

    Conversion of datatype

---
+++ 01_Basic/03_conversionConfusion.js
@@ -0,0 +1,35 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = "KK"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// "33" => 33
+//  "33abc" => NaN (Not a Number)
+//  true => 1 ; false => 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "KK"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true ; 0 =>false
+// ""  => false
+// "KK" => true
+
+let someNumber =33;
+let StringNumber  = String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
