e8051f606b40c88f68a2ca1e468e09f32c9df334
Krantikumar Patil <krantikumar4211@gmail.com>
2025-11-25 07:37:36 +0000

    String

---
+++ 01_Basic/05_string.js
@@ -0,0 +1,33 @@
+const name ="Kranti"
+const repoCount =50
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} and my Repo is ${repoCount}`);
+
+const gameName = new String("Kranti-kk");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+// console.log(gameName.split("-"));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-7,4);
+console.log(anotherString);
+
+const newString1 = " hitesh "
+console.log(newString1);
+console.log(newString1.trim());
+
+
+
+
+
+
