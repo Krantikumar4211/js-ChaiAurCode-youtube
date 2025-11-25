d735bf8d3d8b0f315ffb755a41d94164186e8d64
Krantikumar Patil <krantikumar4211@gmail.com>
2025-11-25 05:56:28 +0000

    Conversion Confusion

--- 01_Basic/03_conversionConfusion.js
+++ 01_Basic/03_conversionConfusion.js
@@ -30,6 +30,57 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber =33;
 let StringNumber  = String(someNumber);
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+// ************************ Operations *********************
+
+let value =3
+let negValue = - value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(3%2);
+// console.log(2/2);
+
+let str1 = "KK"
+let str2 =" Patil"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log(1+ "2");
+// console.log("1"+ 2+ 2);
+// console.log(1+ 2+ "2");
+// console.log(1+ "2"+ 2);
+
+// console.log(true)
+// console.log(+true)
+// console.log(true+)CE
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 =2 + 2  //Not use
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
 
